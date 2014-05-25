86197c22c6057fcea2ad2518d710952a1808a23a
Jacek Lipiec <jacek.b.lipiec@gmail.com>
2014-05-25 19:16:04 +0200

    Basic list done

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -1,13 +1,113 @@
-// Database.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
 #include "database.h"
+#include "stdafx.h"
 
-int main(int argc, char * argv[])
-{
-	DATABASE * db = new DATABASE;
-	delete db;
-	return 0; 
+
+//PErson class
+PERSON::PERSON(){
+	//	items = NULL;
+	next = NULL;
+}
+void PERSON::display(){ // TODO Switch - display with or without items?
+	std::cout << surname.c_str() << "; " << name.c_str() << std::endl;
+}
+PERSON* PERSON::pNext(){ return next; }
+void PERSON::setData(std::string inputName, std::string inputSurname){
+	name = inputName;
+	surname = inputSurname;
 }
 
+//Database class
+DATABASE::DATABASE(){
+	people = NULL;
+	std::cout << "Database created!\n";
+}
+DATABASE::~DATABASE(){ std::cout << "Databse destroyed!\n"; }
+void DATABASE::print(){
+	if (people == NULL){
+		std::cout << "Database is empty!\n";
+		return;
+	}
+	else if (people->pNext() == NULL) {
+		std::cout << "1) ";
+		people->display();
+		return;
+	}
+	PERSON * temp = people;
+	int recordCount = 1;
+	while (temp != NULL) {
+		std::cout << recordCount << ") ";
+		temp->display();
+		temp = temp->pNext();
+		recordCount++;
+	}
+}
+void PERSON::setNext(PERSON * inputNext){
+	next = inputNext;
+}
+void DATABASE::add(std::string inputName, std::string inputSurname){
+	std::cout << "Adding " << inputName.c_str() << " " << inputSurname.c_str() << "...\n";
+	PERSON * newPerson = new PERSON;
+	newPerson->setData(inputName, inputSurname);
+	PERSON * temp = people;
+
+	if (temp != NULL) {
+		// Nodes already present in the list
+		// Parse to end of list
+		while (temp->pNext() != NULL) {
+			temp = temp->pNext();
+		}
+		// Point the last node to the new node
+		temp->setNext(newPerson);
+	}
+	else {
+		// First node in the list
+		people = newPerson;
+	}
+
+}
+
+void DATABASE::remove(){
+
+	// Create a temp pointer
+	PERSON * temp = people;
+
+	// No nodes
+	if (temp == NULL){
+		std::cout << "Database is empty!\n";
+		return;
+	}
+
+	// Last node of the list
+	if (temp->pNext() == NULL) {
+		std::cout << "Deleting: ";
+		temp->display();
+		delete temp;
+		people = NULL;
+		std::cout << "Database cleared!\n";
+	}
+	else {
+		PERSON *previous=NULL;
+		while (temp->pNext() != NULL){
+			previous = temp;
+			temp = temp->pNext();
+		}
+		std::cout << "Deleting: ";
+		temp->display();
+		previous->setNext(NULL);
+		delete temp;
+		
+		//// Parse thru the nodes
+		//PERSON *previous;
+		//do {
+		//	if (tmp->Data() == data) break;
+		//	prev = tmp;
+		//	tmp = tmp->Next();
+		//} while (temp != NULL);
+
+		//// Adjust the pointers
+		//prev->SetNext(tmp->Next());
+
+		//// Delete the current node
+		//delete tmp;
+	}
+}
--- Database/database.h
+++ Database/database.h
@@ -1,8 +1,32 @@
-#include "stdafx.h"
+#include <iostream>
+
+//class ITEM {
+
+
+//};
+
+class PERSON {
+friend class DATABASE;
+private:
+	std::string name;
+	std::string surname;
+	//ITEM * items; // TODO Another singly linked list; this time with items
+	PERSON * next;
+
+	PERSON* pNext();
+	void setData(std::string name, std::string surname);
+	void setNext(PERSON * inputNext);
+	PERSON();
+	void display();
+};
 
 class DATABASE {
 private:
+	PERSON * people;
 public:
-	DATABASE(){ std::cout << "Database created!\n"; }
-	~DATABASE(){ std::cout << "Databse destroyed!\n"; }
+	DATABASE();
+	~DATABASE();
+	void print();
+	void add(std::string name, std::string surname);
+	void remove();//TODO Remove by narrowing down people to one record, or by id. Currently employed: Delete last
 };
