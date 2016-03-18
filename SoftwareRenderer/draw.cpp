8a328ed54993b2429414c3f80b5d53ed78d51099
Venthe <jacek.b.lipiec@gmail.com>
2016-03-18 16:18:42 +0100

    Fixed Bersenham's line algorithm. Added DrawObj for drawing a wire.

--- SoftwareRenderer/draw.cpp
+++ SoftwareRenderer/draw.cpp
@@ -1,5 +1,6 @@
 ﻿#include "config.h"
 #include "draw.h"
+#include "file_handler.h"
 #include <random>
 
 void _PutPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b)
@@ -45,7 +46,7 @@ void _MyFillRect(int x, int y, int maxX, int maxY, unsigned char r, unsigned cha
 			for (int width = x; width < maxX; width++) { // Width
 				*windowbuffer++ = (r << 16) + (g << 8) + (b);
 			}
-			windowbuffer += settings.bufferSizeX-deltaX;
+			windowbuffer += settings.bufferSizeX - deltaX;
 		}
 	}
 }
@@ -98,16 +99,16 @@ void DrawLine(int x, int y, int tx, int ty, unsigned char r, unsigned char g, un
 	}
 
 	double error = 0.0f; //	real error : = 0
-	double deltaerr = (double) deltay / (double) deltax; //	real deltaerr : = abs(deltay / deltax)
+	double deltaerr = (double)deltay / (double)deltax; //	real deltaerr : = abs(deltay / deltax)
 	if (deltaerr < 0.0) deltaerr *= -1.0;
 
 	int cury = y; //	int y : = y0
 
 	for (int curx = x; curx != tx;) { // for x from x0 to x1
-		_PutPixel(curx, cury,r,g,b); // plot(x, y)
+		_PutPixel(curx, cury, r, g, b); // plot(x, y)
 		error += deltaerr; // error : = error + deltaerr
-		while (error >= 0.5 && cury!=ty) { // while error ≥ 0.5 then
-			_PutPixel(curx, cury,r,g,b); // plot(x, y)
+		while (error >= 0.5 && cury != ty) { // while error ≥ 0.5 then
+			_PutPixel(curx, cury, r, g, b); // plot(x, y)
 			if (deltay > 0) { cury++; } // y : = y + sign(y1 - y0)
 			else { cury--; }
 			error -= 1.0; // error := error - 1.0
@@ -116,15 +117,64 @@ void DrawLine(int x, int y, int tx, int ty, unsigned char r, unsigned char g, un
 		else { curx--; }
 	}
 }
-void DrawCircle(int x, int y, int radius,double step, unsigned char r, unsigned char g, unsigned char b)
+void DrawCircle(int x, int y, int radius, double step, unsigned char r, unsigned char g, unsigned char b)
 {
 	const double pi = 3.14159265358979323846;
-	for (double angle = 0.0; angle < 360.0;angle += step)
-		_PutPixel(x+radius*sin((angle / 360.0)*2*pi), y+radius*cos((angle / 360.0) * 2 *pi), r, g, b);
+	for (double angle = 0.0; angle < 360.0; angle += step)
+		_PutPixel(x + radius*static_cast<int>(sin((angle / 360.0)*2.0*pi)), y + radius*static_cast<int>(cos((angle / 360.0) * 2.0 *pi)), r, g, b);
 }
 void DrawFan(int x, int y, int radius, double step, unsigned char r, unsigned char g, unsigned char b)
 {
 	const double pi = 3.14159265358979323846;
 	for (double angle = 0.0; angle < 360.0; angle += step)
-		DrawLine(x, y, x + radius*sin((angle/360.0)*2*pi), y + radius*cos((angle / 360.0)*2*pi), r, g, b);
+		DrawLine(x, y, x + radius*static_cast<int>(sin((angle / 360.0)*2.0*pi)), y + radius*static_cast<int>(cos((angle / 360.0)*2.0*pi)), r, g, b);
+}
+
+void DrawGrid(int density, unsigned char r, unsigned char g, unsigned char b)
+{
+
+	for (int i = 0; i < ((settings.bufferSizeX) / density) + 1; i++) {
+		DrawLine(i * density, 0, i * density, settings.bufferSizeY, r, g, b);
+		DrawLine(0, i * density, settings.bufferSizeX, i * density, r, g, b);
+	}
+}
+
+void DrawObj(ObjModel model, ThreeTuple<double> ShiftOrigin)
+{
+
+	//Test PointCloud
+	//for each (ThreeTuple<double> v in model.Vertex)
+	//{
+	//	_PutPixel((settings.bufferSizeX/2)+(v[0] * model.Scale[0]), (settings.bufferSizeY / 2)+200+(-v[1] * model.Scale[1]),0xff,0xff,0xff);
+	//}
+
+	for (unsigned int i = 0; i < model.Face.size(); i++)
+	{
+		//TODO: Z depth. Temp solution? Draw half of the model
+		if (
+			static_cast<int>(model.Vertex.at(model.Face.at(i)[0] - 1)[2] * model.Scale[2] + ShiftOrigin[2]) < 0.0 ||
+			static_cast<int>(model.Vertex.at(model.Face.at(i)[1] - 1)[2] * model.Scale[2] + ShiftOrigin[2]) < 0.0 ||
+			static_cast<int>(model.Vertex.at(model.Face.at(i)[2] - 1)[2] * model.Scale[2] + ShiftOrigin[2]) < 0.0)
+		{
+			continue;
+		}
+		DrawLine(
+			static_cast<int>(model.Origin[0] + ShiftOrigin[0] + model.Vertex.at(model.Face.at(i)[0] - 1)[0] * model.Scale[0]),
+			static_cast<int>(model.Origin[1] + ShiftOrigin[1] + model.Vertex.at(model.Face.at(i)[0] - 1)[1] * model.Scale[1] * -1.0),
+			static_cast<int>(model.Origin[0] + ShiftOrigin[0] + model.Vertex.at(model.Face.at(i)[1] - 1)[0] * model.Scale[0]),
+			static_cast<int>(model.Origin[1] + ShiftOrigin[1] + model.Vertex.at(model.Face.at(i)[1] - 1)[1] * model.Scale[1] * -1.0)
+			); // v1 to v2
+		DrawLine(
+			static_cast<int>(model.Origin[0] + ShiftOrigin[0] + model.Vertex.at(model.Face.at(i)[1] - 1)[0] * model.Scale[0]),
+			static_cast<int>(model.Origin[1] + ShiftOrigin[1] + model.Vertex.at(model.Face.at(i)[1] - 1)[1] * model.Scale[1] * -1.0),
+			static_cast<int>(model.Origin[0] + ShiftOrigin[0] + model.Vertex.at(model.Face.at(i)[2] - 1)[0] * model.Scale[0]),
+			static_cast<int>(model.Origin[1] + ShiftOrigin[1] + model.Vertex.at(model.Face.at(i)[2] - 1)[1] * model.Scale[1] * -1.0)
+			); // v2 to v3
+		DrawLine(
+			static_cast<int>(model.Origin[0] + ShiftOrigin[0] + model.Vertex.at(model.Face.at(i)[2] - 1)[0] * model.Scale[0]),
+			static_cast<int>(model.Origin[1] + ShiftOrigin[1] + model.Vertex.at(model.Face.at(i)[2] - 1)[1] * model.Scale[1] * -1.0),
+			static_cast<int>(model.Origin[0] + ShiftOrigin[0] + model.Vertex.at(model.Face.at(i)[0] - 1)[0] * model.Scale[0]),
+			static_cast<int>(model.Origin[1] + ShiftOrigin[1] + model.Vertex.at(model.Face.at(i)[0] - 1)[1] * model.Scale[1] * -1.0)
+			); // v3 to v1
+	}
 }
