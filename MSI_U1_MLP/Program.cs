dea94cc2dac10dd3218677db7628e616e75a947a
Venthe <jacek.b.lipiec@gmail.com>
2017-11-20 14:24:15 +0100

    Add project files.

---
+++ MSI_U1_MLP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSI_U1_MLP
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+      }
+   }
+}
