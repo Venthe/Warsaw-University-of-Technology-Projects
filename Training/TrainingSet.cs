de6a8168cf062887448992f72668f221de421646
Venthe <jacek.b.lipiec@gmail.com>
2017-11-26 20:37:07 +0100

    Do minor cleanup

--- Training/TrainingSet.cs
+++ Training/TrainingSet.cs
@@ -9,12 +9,10 @@ namespace MultiLayeredPerceptron.Training {
 
       public TrainingSet(int entriesCount, IList<string> labels) {
          for (var i = 0; i < entriesCount; i++) {
-            //TODO :)
+            // TODO Training set is now statically set to very simple problem (x, y, is it above the line) and thus it's constructor is garbage
             this.TrainingEntries.Add(new AboveLineTrainingEntry(DoubleExtension.GetRandomNumber(-10, 10), DoubleExtension.GetRandomNumber(-10, 10), labels[0]));
          }
       }
-
-      public ITrainingEntry GetTrainingEntry(int index) => this.TrainingEntries[index];
 
       public override string ToString() => ListExtensions.ToString(this.TrainingEntries);
    }
