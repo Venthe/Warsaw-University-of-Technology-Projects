945bd770c6e09870764bb53bc9ebdceefc1571e7
Venthe <jacek.b.lipiec@gmail.com>
2018-06-25 14:35:35 +0200

    Add dashboard stub
    * Added bootstrap package

--- Bookstore/Global.asax.cs
+++ Bookstore/Global.asax.cs
@@ -10,14 +10,26 @@ using System.Web.Http;
 
 namespace Bookstore
 {
-    public class Global : HttpApplication
-    {
-        void Application_Start(object sender, EventArgs e)
-        {
-            // Code that runs on application startup
-            AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
-        }
-    }
+   public class Global : HttpApplication
+   {
+      void Application_Start(object sender, EventArgs e)
+      {
+         // Code that runs on application startup
+         AreaRegistration.RegisterAllAreas();
+
+         RegisterRoutes(RouteTable.Routes);
+      }
+
+      public static void RegisterRoutes(RouteCollection routes)
+      {
+         routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+         routes.MapRoute(
+             "Default",                                              // Route name 
+             "{controller}/{action}/{id}",                           // URL with parameters 
+             new { controller = "Dashboard", action = "Index", id = "" }  // Parameter defaults
+         );
+
+      }
+   }
 }
