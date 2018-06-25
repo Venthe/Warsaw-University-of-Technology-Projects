b293ace41cb89490188e07037f0ba72045e9d131
Venthe <jacek.b.lipiec@gmail.com>
2018-06-25 17:29:39 +0200

    Add navigation menu

---
+++ Bookstore/Controllers/HeaderController.cs
@@ -0,0 +1,30 @@
+﻿using Bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bookstore.Controllers
+{
+   public class HeaderController : Controller
+   {
+      public ActionResult Navigation()
+      {
+         var model = new List<NavigationModel>
+         {
+            new NavigationModel() { Action = "Index", Controller = "Login", Title = "Login" },
+            new NavigationModel() { Action = "Index", Controller = "Dashboard", Title = "Dashboard" }
+         };
+
+         return PartialView("_Navigation", model);
+      }
+
+      public ActionResult Index()
+      {
+         var model = "Bookstore";
+
+         return PartialView("_Index", model);
+      }
+   }
+}
---
+++ Bookstore/Models/NavigationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookstore.Models
+{
+   public class NavigationModel
+   {
+      public String Title { get; set; }
+      public String Action { get; set; }
+      public String Controller { get; set; }
+   }
+}
