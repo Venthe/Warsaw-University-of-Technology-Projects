4a0b39a6ceffab9d2ad085244d174168cf8eceba
Venthe <jacek.b.lipiec@gmail.com>
2018-06-25 16:55:02 +0200

    Add dummy login action

---
+++ Bookstore/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bookstore.Controllers
+{
+   public class LoginController : Controller
+   {
+      // GET: Login
+      public ActionResult Index()
+      {
+         return View();
+      }
+
+      // POST: Login
+      [HttpPost]
+      public ActionResult Index(FormCollection collection)
+      {
+         try
+         {
+            // TODO: Add insert logic here
+            var userModel = new UserModel()
+            {
+               Username = collection.Get(0)
+            };
+
+            return RedirectToAction("Index", "Dashboard");
+         }
+         catch
+         {
+            return View();
+         }
+      }
+   }
+}
---
+++ Bookstore/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookstore.Models
+{
+   public class UserModel
+   {
+      // TODO: Move to BE
+      public String Username
+      {
+         get => HttpContext.Current.Application["username"] as String;
+         set { HttpContext.Current.Application["username"] = value; }
+      }
+   }
+}
